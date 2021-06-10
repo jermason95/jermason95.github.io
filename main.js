7c7f5d0d2ab89a89dfb7a2c80cad70f9bbb8c0e4
Jeremy Mason <jermason95@gmail.com>
2021-06-10 18:26:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -154,7 +154,9 @@ for (i=0; i<divDetails.length; i++) {
 }
 
 // DOWNLOAD RESUME BUTTON
-//function goToDownload() {
+function onclick= href="https://www.youtube.com/watch?v=1x1u4PWbNYY" target="_blank"
+//href="https://www.youtube.com/watch?v=1x1u4PWbNYY" target="_blank">
+//goToDownload() {
     //window.scrollTo(0,document.body.scrollHeight);
     // INDEX 14 IS THE DOWNLOAD SVG, MAY HAVE TO CHANGE THIS LATER
    // svgs[14].style.animation = "2s linear 1s 8 alert";
